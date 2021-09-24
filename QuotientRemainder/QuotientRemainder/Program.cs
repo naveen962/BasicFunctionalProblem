186dfd85f8f1887caa5123e0c82ae35031ea14a9
naveen962 <mrn28897@gmail.com>
2021-09-24 05:09:40 -0700

    Quotient and Remainder program is added

---
+++ QuotientRemainder/QuotientRemainder/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace QuotientRemainder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //taking user inputs
+            Console.Write("Enter the Divisor = ");
+            int Divisor = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the Dividend = ");
+            int Dividend = int.Parse(Console.ReadLine());
+            //computation
+            int Quotient = Dividend / Divisor;
+            Console.WriteLine("Quotient = " +Quotient);
+            int Remainder = Dividend % Divisor;
+            Console.WriteLine("Remainder = " +Remainder);
+        }
+    }
+}
---
+++ QuotientRemainder/SwapTwoNos/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SwapTwoNos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //taking user inputs
+            Console.Write("Please Enter The First Number = ");
+            int FirstNumber = int.Parse(Console.ReadLine());
+            Console.Write("Please Enter The Second Number = ");
+            int SecondNumber = int.Parse(Console.ReadLine());
+            //declare variable
+            int temp = 0;
+            //swapping 
+            temp = FirstNumber;
+            FirstNumber = SecondNumber;
+            SecondNumber = temp;
+            Console.WriteLine("First Number = " + FirstNumber);
+            Console.WriteLine("Second Number = " + SecondNumber);
+        }
+    }
+}
