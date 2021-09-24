ff27474655ed619a2a750f2cd5c1b4b3b5cb7ef9
naveen962 <mrn28897@gmail.com>
2021-09-24 05:17:49 -0700

    Odd or Even program is added

---
+++ EvenOdd/EvenOdd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace EvenOdd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the Number:");
+            int num = int.Parse(Console.ReadLine());
+            if (num%2==0)
+            {
+                Console.WriteLine(num + "is EVEN");
+            }
+            else
+            {
+                Console.WriteLine(num + "is ODD");
+            }
+        }
+    }
+}
