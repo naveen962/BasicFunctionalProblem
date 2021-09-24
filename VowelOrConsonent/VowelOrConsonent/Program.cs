96683debffc580662daa49c3b8d8a2ed62d63ee9
naveen962 <mrn28897@gmail.com>
2021-09-24 05:20:26 -0700

    vowel and consonent program is added

---
+++ VowelOrConsonent/VowelOrConsonent/Program.cs
@@ -0,0 +1,36 @@
+﻿
+using System;
+
+namespace VowelOrConsonant
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Alphabet : ");
+            char Alpha = Console.ReadLine()[0];
+            if ((Alpha >= 'A' && Alpha <= 'Z') || (Alpha >= 'a' && Alpha <= 'z'))
+            {
+                if (Alpha == 'A' || Alpha == 'E' || Alpha == 'I' || Alpha == 'O' || Alpha == 'U' || Alpha == 'a' || Alpha == 'e' || Alpha == 'i' || Alpha == 'o' || Alpha == 'u')
+                {
+                    Console.WriteLine("VOWEL");
+                }
+                else
+                {
+                    Console.WriteLine("CONSONANT");
+                }
+            }
+            else
+            {
+                Console.WriteLine("NOT AN ALPHABET");
+
+            }
+        }
+    }
+}
+
+
+
+
+
+
