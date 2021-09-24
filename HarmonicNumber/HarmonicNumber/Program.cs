1cd3d397912c07426984984f6551aaefdf47efc4
naveen962 <mrn28897@gmail.com>
2021-09-24 05:03:06 -0700

    Harmonic Number program is added

---
+++ HarmonicNumber/HarmonicNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HarmonicNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter the no of terms : ");
+            int n = int.Parse(Console.ReadLine());
+            float s = 0.0f;
+           
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write("1/{0} + ", i);
+                s += 1 / (float)i;
+            }
+            Console.Write("\nSum of Series upto {0} terms : {1} \n", n, s);
+        }
+    }
+}
