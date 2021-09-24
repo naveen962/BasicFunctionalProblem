395ddef09007a1d66adb135f662698fb0838c2c1
naveen962 <mrn28897@gmail.com>
2021-09-24 05:06:23 -0700

    Prime Factorization program is added

---
+++ PrimeFactor/PrimeFactor/Prime.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PrimeFactor
+{
+    class Prime
+    {
+        public static void PrimeFact()
+        {
+            Console.WriteLine("Please enter the Number!!!");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine("The Prime Factors are:");
+            int i = 2;
+            
+            while (num>1)
+                {
+                    if (num % i == 0)
+                    {
+                   
+                    Console.WriteLine(i);
+                    num = num / i;
+                    }
+                    else
+                    {
+                        i++;
+                    }
+                }
+        }
+    }
+}
---
+++ PrimeFactor/PrimeFactor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PrimeFactor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Prime.PrimeFact();
+        }
+    }
+}
