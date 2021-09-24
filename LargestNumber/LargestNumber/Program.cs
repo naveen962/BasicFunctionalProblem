ca2620e7274e33d99d1bc120a5d12db219ee15f7
naveen962 <mrn28897@gmail.com>
2021-09-24 05:25:33 -0700

    finding Largest number between 3 Numbers program is added

---
+++ LargestNumber/LargestNumber/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter 3 Numbers");
+            int n1 = int.Parse(Console.ReadLine());
+            int n2 = int.Parse(Console.ReadLine());
+            int n3 = int.Parse(Console.ReadLine());
+
+            if (n1>n2 && n1>n3)
+            {
+                Console.WriteLine(n1+" is the largest");
+
+            }
+            else if (n2 > n1 && n2 > n3)
+            {
+                Console.WriteLine(n2+" is the largest");
+
+            }
+            else
+            {
+                Console.WriteLine(n3+" is the largest");
+
+            }
+        }
+       
+    }
+}
